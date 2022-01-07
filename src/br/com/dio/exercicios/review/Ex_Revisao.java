bef85d86e864367cb201413b424167b3e820db7d
Lenonnn <lenonpolly@gmail.com>
2022-01-07 11:50:03 -0300

    Finalizando aulas de revisão de laços e arrays

--- src/br/com/dio/exercicios/review/Ex_Revisao.java
+++ src/br/com/dio/exercicios/review/Ex_Revisao.java
@@ -25,18 +25,78 @@ public class Ex_Revisao {
         idades = new int[] {12, 35, 45, 29 ,9, 37, 51, 61};
 
         for ( int idade : idades ) {
-            System.out.println("Idade: " + idade);
+            System.out.println("Idade * : " + idade);
+        }
+
+        for ( int a = 0; a < idades.length; a++){
+            int idade = idades[a];
+            System.out.println("Idade : " + idade);
+        }
+
+        for ( int idade : idades ) {
+            if ( idade >= 18 )
+            System.out.println("Idade - : " + idade);
         }
 
         for ( int a = 0; a < idades.length; a++){
             int idade = idades[a];
             if ( idade >= 18 ) {
-                System.out.println("Idade " + idade + " é maior que 18 anos");
+                System.out.println("Idade / " + idade + " é maior que 18 anos");
             }
             else {
                 System.out.println("Com " + idade + " anos, é menor de idade!");
             }
         }
 
+        long[][] m = new long[3][3];
+
+        char[][] jogo = new char[3][3];
+        jogo[0][0] = 'X';
+        jogo[2][1] = 'O';
+        System.out.println("Posição 0 " + jogo[0][0]);
+        System.out.println("Posição 8 " + jogo[2][1]);
+
+        int[][][] n = new int[3][3][3];
+        n[0][0][0] = 10;
+        n[1][1][0] = 20;
+
+        //System.out.println("Array n multidimensional" + n);
+
+        int[][] b = new int[][]{{1,2},{3,4}};
+        int[][] l = {{5,6},{7,8}};
+        int[][][] t = new int[][][]{ {{1,2},{3,4}}, {{5,6},{7,8}} };
+        int[][][] x = { {{1,2},{3,4}}, {{5,6},{7,8}} };
+
+        int[][] idades_ = new int[][]{{12,35},{45,22}};
+        /*  12  35
+            45  22   */
+
+        for (int[] id1 : idades_ ) {
+            for (int id2 : id1 ){
+                System.out.println(id2);
+            }
+        }
+
+        for (int c = 0; c < idades_.length; c++){
+            // define id para cada loop olhando C -> id = [12,35] ; id = [45,22];
+            int[] id = idades_[c];
+            for (int d = 0; d < id.length; d++){ // 0 < id( 1 ) - imprime
+                System.out.println("Age: " + idades_[c][d]);//[0][0];
+            }
+        }
+
+        byte[] bytes = new byte[2];
+        bytes[1] = 100;
+
+        float[] floats = new float[2];
+        floats[1] = 10.98f;
+
+        for (byte byte_ : bytes ) {
+            System.out.println("byte: " + byte_);
+        }
+
+        for (float float_ : floats ) {
+            System.out.println("float: " + float_);
+        }
     }
 }
